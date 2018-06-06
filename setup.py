69dc14d7472a3340226c27f0addc5b1b7eebf578
Marco Muratore <marco.muratore@edo.io>
2018-06-06 19:04:03 +0200

    bump version and dependencies

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     author_email='nikita.marchant@gmail.com',
     install_requires=[
         "python-dateutil",
-        "arrow==0.4.2",
+        "arrow==0.12.1",
         "six>1.5",
     ],
     license=__license__,
